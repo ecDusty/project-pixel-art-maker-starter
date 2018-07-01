4fc99abfebca30c71a7329b984df27c3fdc5384e
ecDusty <e.macalligan@outlook.com>
2018-07-01 23:15:32 +1200

    ADD: makeGrid function

--- src/js/designs.js
+++ src/js/designs.js
@@ -6,12 +6,30 @@
 function makeGrid() {
 
 // Your code goes here!
-    const gHeight = $('#inputHeight').val();
-    const gWidth = $('#inputWidth').val();
+    const gHeight = document.getElementById('inputHeight').value;
+    const gWidth = document.getElementById('inputWidth').value;
+    const pixCanvas = document.getElementById('pixelCanvas');
+    let divEl = document.createElement('div');
+
+    do {
+        let i = 0;
+    } while (i < gHeight) {
+        let pixRow = divEl;
+        pixRow.className = 'pixRow';
+
+        for(let ii = 0; ii < gWidth; ii++) {
+            let pixBlock = divEl;
+            pixBlock.className = 'pixBlock';
+            pixRow.appendChild(pixBlock);
+
+        }
+
+        pixCanvas.appendChild(pixRow);
+        
+        i++
+    }
 
 }
 
-$('form input[type="submit"]').click(function(e) {
-    e.preventDefault();
-    makeGrid();
-});
+const button = document.getElementById('Submit');
+button.onclick(makeGride);
