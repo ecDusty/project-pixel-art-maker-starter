1893896d77f58f624af86c1591402ff74ad0dac7
ecDusty <e.macalligan@outlook.com>
2018-07-02 00:03:26 +1200

    ADD: Changebackground function

--- src/js/designs.js
+++ src/js/designs.js
@@ -1,21 +1,26 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-    const gHeight = document.getElementById('inputHeight').value;
-    const gWidth = document.getElementById('inputWidth').value;
+    //Get the Values & elements
+    const gHeight = parseInt(document.getElementById('inputHeight').value);
+    const gWidth = parseInt(document.getElementById('inputWidth').value);
+    const pColor = document.getElementById('colorPicker');
     const pixCanvas = document.getElementById('pixelCanvas');
 
+    //Reset the Canvas
     pixCanvas.innerHTML = '';
+
+    //Function that sets background color
+    function setColor(el) {
+        this.style.backgroundColor = pColor.value;
+    }
+
+    //This makes sure the canvas is mobile responsive
     pixCanvas.style.maxWidth = (gWidth * 20 + gWidth + 1) + `px`;
     pixCanvas.style.width = `100%`;
 
+    //Create the pixel blocks.
     let i = 0;
-    
     while (i < gHeight) {
         let pixRow = document.createElement('div');
         pixRow.className = 'pixRow';
@@ -23,17 +28,18 @@ function makeGrid() {
         for(let ii = 0; ii < gWidth; ii++) {
             let pixBlock = document.createElement('div');
             pixBlock.className = 'pixBlock';
+            pixBlock.addEventListener('click',setColor);
             pixRow.appendChild(pixBlock);
-
         }
 
         pixCanvas.appendChild(pixRow);
-        
         i++
     }
 
 }
 
+
+// Grab the action button, and link it to making the grid. Always make sure to prevent default, so the page doesn't reload.
 const button = document.getElementById('Submit');
 button.addEventListener('click', function(e) {
     
